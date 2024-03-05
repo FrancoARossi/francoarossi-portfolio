7de19e2a60ce3f0d64eee4c11936638187450058
FrancoARossi <rossifrancoa@gmail.com>
2024-03-05 14:01:47 -0300

    feat: hero easter egg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,10 +5,17 @@ export default {
     extend: {
       animation: {
         gradient: "gradient 3s linear infinite",
+        shake: "shake 0.5s cubic-bezier(.36,.07,.19,.97)",
       },
       keyframes: {
         gradient: {
           to: { "background-position": "200% center" },
+        },
+        shake: {
+          "10%, 90%": { transform: "translate3d(-1px, 0, 0)" },
+          "20%, 80%": { transform: "translate3d(2px, 0, 0)" },
+          "30%, 50%, 70%": { transform: "translate3d(-4px, 0, 0)" },
+          "40%, 60%": { transform: "translate3d(4px, 0, 0)" },
         },
       },
     },
