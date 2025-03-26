0d6424858b1bbb17575819b63ee52264eebe55d5
FrancoARossi <rossifrancoa@gmail.com>
2025-03-26 14:53:42 -0300

    chore: code improvements

---
+++ src/utils/constants.ts
@@ -0,0 +1,81 @@
+export const colorClassesByTechnology: { [technology: string]: string } = {
+  JavaScript:
+    "border-yellow-500 bg-yellow-200 dark:bg-yellow-950 dark:border-yellow-800",
+
+  TypeScript:
+    "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  React:
+    "border-indigo-500 bg-indigo-200 dark:bg-indigo-950 dark:border-indigo-800",
+
+  Redux:
+    "border-purple-500 bg-purple-200 dark:bg-purple-950 dark:border-purple-800",
+
+  ReactNative:
+    "border-indigo-500 bg-indigo-200 dark:bg-indigo-950 dark:border-indigo-800",
+
+  Expo: "border-indigo-500 bg-indigo-200 dark:bg-indigo-950 dark:border-indigo-800",
+
+  Firebase:
+    "border-yellow-500 bg-yellow-200 dark:bg-yellow-950 dark:border-yellow-800",
+
+  Strapi: "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  MUI: "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  RSpec: "border-red-500 bg-red-200 dark:bg-red-950 dark:border-red-800",
+
+  Nodejs:
+    "border-green-500 bg-green-200 dark:bg-green-950 dark:border-green-800",
+
+  Express:
+    "border-green-500 bg-green-200 dark:bg-green-950 dark:border-green-800",
+
+  Arduino: "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  "C/C++": "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  Python:
+    "border-yellow-500 bg-yellow-200 dark:bg-yellow-950 dark:border-yellow-800",
+
+  Linux:
+    "border-black-500 bg-black-200 dark:bg-black-950 dark:border-black-800",
+
+  GraphQL: "border-pink-500 bg-pink-200 dark:bg-pink-950 dark:border-pink-800",
+
+  PostgreSQL:
+    "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  MongoDB:
+    "border-green-500 bg-green-200 dark:bg-green-950 dark:border-green-800",
+
+  Redis: "border-red-500 bg-red-200 dark:bg-red-950 dark:border-red-800",
+
+  Docker: "border-blue-500 bg-blue-200 dark:bg-blue-950 dark:border-blue-800",
+
+  RubyOnRails: "border-red-500 bg-red-200 dark:bg-red-950 dark:border-red-800",
+
+  Ruby: "border-red-500 bg-red-200 dark:bg-red-950 dark:border-red-800",
+
+  HTML: "border-orange-500 bg-orange-200 dark:bg-orange-950 dark:border-orange-800",
+
+  CSS: "border-sky-500 bg-sky-200 dark:bg-sky-950 dark:border-sky-800",
+
+  TailwindCSS:
+    "border-cyan-500 bg-cyan-200 dark:bg-cyan-950 dark:border-cyan-800",
+
+  Sass: "border-pink-500 bg-pink-200 dark:bg-pink-950 dark:border-pink-800",
+
+  Jest: "border-rose-500 bg-rose-200 dark:bg-rose-950 dark:border-rose-800",
+
+  Git: "border-rose-500 bg-rose-200 dark:bg-rose-950 dark:border-rose-800",
+
+  GitHub:
+    "border-violet-500 bg-yviolet-200 dark:bg-violet-950 dark:border-violet-800",
+
+  GitLab:
+    "border-amber-500 bg-amber-200 dark:bg-amber-950 dark:border-amber-800",
+
+  default:
+    "border-black-500 bg-black-200 dark:bg-black-950 dark:border-black-800",
+};
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -7,3 +7,51 @@ export const yearsOfExperience = () => {
 
   return yearsOfExperience;
 };
+
+export const getTechnologyProps = (technology: string) => {
+  switch (technology) {
+    case "TailwindCSS":
+      return {
+        colorClasses:
+          "border-cyan-500 bg-cyan-200 dark:bg-cyan-950 dark:border-cyan-800",
+        label: "Tailwind CSS",
+      };
+    case "Sass":
+      return {
+        colorClasses:
+          "border-pink-500 bg-pink-200 dark:bg-pink-950 dark:border-pink-800",
+        label: "Sass",
+      };
+    case "Jest":
+      return {
+        colorClasses:
+          "border-rose-500 bg-rose-200 dark:bg-rose-950 dark:border-rose-800",
+        label: "Jest",
+      };
+    case "Git":
+      return {
+        colorClasses:
+          "border-rose-500 bg-rose-200 dark:bg-rose-950 dark:border-rose-800",
+        label: "Git",
+      };
+    case "GitHub":
+      return {
+        colorClasses:
+          "border-violet-500 bg-yviolet-200 dark:bg-violet-950 dark:border-violet-800",
+        label: "GitHub",
+      };
+    case "GitLab":
+      return {
+        colorClasses:
+          "border-amber-500 bg-amber-200 dark:bg-amber-950 dark:border-amber-800",
+        label: "GitLab",
+      };
+
+    default:
+      return {
+        colorClasses:
+          "border-black-500 bg-black-200 dark:bg-black-950 dark:border-black-800",
+        label: "default",
+      };
+  }
+}
