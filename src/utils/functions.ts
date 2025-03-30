c372c128386ae77bc5da1ef43f0fc178be6ffad4
FrancoARossi <rossifrancoa@gmail.com>
2025-03-30 11:56:49 -0300

    feat: add accessibility improvements

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { EASTER_EGGS } from "./constants";
+
 export const yearsOfExperience = () => {
   const startDateMillis = new Date("2020-11-01").getTime();
   const currentDateMillis = new Date().getTime();
@@ -9,17 +11,30 @@ export const yearsOfExperience = () => {
 };
 
 export const completeEasterEgg = (easterEgg: string) => {
-  let easterEggs = localStorage.getItem("foundEasterEggs")?.split(",") || [];
+  let foundEasterEggs =
+    localStorage.getItem("foundEasterEggs")?.split(",") || [];
 
-  if (!easterEggs.includes(easterEgg)) {
-    easterEggs.push(easterEgg);
-    localStorage.setItem("foundEasterEggs", easterEggs.join(","));
+  if (!foundEasterEggs.includes(easterEgg)) {
+    foundEasterEggs.push(easterEgg);
+    localStorage.setItem("foundEasterEggs", foundEasterEggs.join(","));
     const foundEasterEggsCount = document.getElementById(
       "found-easter-eggs-count",
     );
     if (foundEasterEggsCount) {
       foundEasterEggsCount.innerText =
-        String(easterEggs.filter(Boolean).length) || "0";
+        String(foundEasterEggs.filter(Boolean).length) || "0";
+      updateEasterEggsIcon();
     }
+  }
+};
+
+export const updateEasterEggsIcon = () => {
+  const easterEggsIcon = document.getElementById("easter-eggs-icon");
+  let foundEasterEggs =
+    localStorage.getItem("foundEasterEggs")?.split(",") || [];
+
+  if (easterEggsIcon && foundEasterEggs) {
+    easterEggsIcon.innerHTML =
+      foundEasterEggs.length === EASTER_EGGS.length ? "🎉" : "👀";
   }
 };
